3a8551362377db777e9599654e8082a681188589
juhyunju <juhyunju@naver.com>
2022-01-06 14:27:01 +0900

    에러 처리 미들웨어

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const app = express();
 const port = 3000
 
 app.use('/',(req,res,next) => {
-    console.log('미들웨어입다')
+    console.log('모든 요청에 다 실행됩니다.')
     next()
 })
-app.get('/', (req,res) => {
-    res.send('Hello, Express!')
+app.get('/', (req,res,next) => {
+    console.log('GET / 요청에서만 실행됩니다.')
+    next()
+}, (req,res) => {
+    throw new Error('에러는 에러 처리 미들웨어로 갑니다')
 })
-app.post('/',(req,res) => {
-    console.log('post실행')
-    res.send('post다')
+app.use((err,req,res,next) => {
+    console.error(err)
+    res.status(500).send(err.message)
 })
-
 app.listen(port, () => {
     console.log(`${port}번 포트에서 실행중!`)
 })
